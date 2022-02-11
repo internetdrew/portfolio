4263ba8fb0c4b5d14e358301cae202172eaed7bf
Andrew Rowley <andrew@internetdrew.com>
2022-02-11 13:01:01 -0500

    Add scroll to top functionality

---
+++ app.js
@@ -0,0 +1,17 @@
+const topButton = document.getElementById('top-arrow');
+
+function toggleTopButton() {
+  if (document.documentElement.scrollTop <= 150) {
+    topButton.style.display = 'none';
+  } else {
+    topButton.style.display = 'block';
+  }
+}
+toggleTopButton();
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+topButton.addEventListener('click', scrollToTop);
+document.addEventListener('scroll', toggleTopButton);
